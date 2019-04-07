9123ff325c534b835cd689609182d078c1d3bca6
Aayushjn <aayushjn11@gmail.com>
2019-04-07 15:35:03 +0530

    Added app logo. Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.21")
-    implementation("androidx.appcompat:appcompat:1.1.0-alpha03")
+    implementation("androidx.appcompat:appcompat:1.1.0-alpha04")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
 
     implementation("com.google.android.material:material:1.0.0")
@@ -39,6 +39,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.1")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.0")
-    androidTestImplementation("androidx.test:runner:1.1.2-alpha02")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0-alpha02")
+    androidTestImplementation("androidx.test:runner:1.2.0-alpha03")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0-alpha03")
 }
